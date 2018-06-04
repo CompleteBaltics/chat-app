6b8d1011f2ebe27aabb6aacb9239704df1f3d535
Caspar Skripkauskas <caspar@completewithus.com>
2018-06-04 19:36:10 +0300

    get and send message

---
+++ public/scripts/index.js
@@ -0,0 +1,18 @@
+var socket = io();
+
+socket.on('connect', function() {
+  console.log('Connected');
+
+  socket.emit('createMessage', {
+    from: 'ok',
+    text: 'DASASFASFDASF'
+  });
+
+  socket.on('newMessage', function(message){
+    console.log(message);
+  });
+});
+
+socket.on('disconnect', function() {
+  console.log('disconected from server');
+});
--- server/server.js
+++ server/server.js
@@ -1,12 +1,36 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 
 let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+
 app.use(express.static(publicPath));
-app.listen(port, () => {
+
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('createMessage', (newMessage) => {
+    console.log(newMessage);
+  });
+
+  socket.emit('newMessage', {
+    from: 'asdfsf',
+    text: 'sadfsdfsadf',
+    createdAt: Date.now()
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User was disconected');
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server up on port ${port}`);
 });
 
